7f0827a7b5ad317a52528c81c0f39f2f064e0eb6
monishramadoss <monish.ramadoss@outlook.com>
2020-09-25 12:12:16 -0700

    config fixe

--- coronary_calcium/jmodels/Coronary_Calcium.py
+++ coronary_calcium/jmodels/Coronary_Calcium.py
@@ -61,25 +61,25 @@ if(not os.path.isdir(log_dir)):
 
 def dense_unet(inputs, filters=32):
     '''Model Creation'''
-    #Define model#
-    # Define kwargs dictionary#
+    # Define kwargs dictionary
     kwargs = {
-    'kernel_size': (1,3,3),
-    'padding': 'same',
-    'bias_initializer':'zeros'} #zeros, ones, golorit_uniform
-    #Define lambda functions#
+        'kernel_size': (1,3,3),
+        'padding': 'same',
+        'bias_initializer':'zeros'
+    } 
+    # Define lambda functions#
     conv = lambda x, filters, strides : layers.Conv3D(filters=int(filters), strides=(1, strides, strides), **kwargs)(x)
     norm = lambda x : layers.BatchNormalization()(x)
     relu = lambda x : layers.LeakyReLU()(x)
-    #Define stride-1, stride-2 blocks#
+    # Define stride-1, stride-2 blocks#
     conv1 = lambda filters, x : relu(norm(conv(x, filters, strides=1)))
     conv2 = lambda filters, x : relu(norm(conv(x, filters, strides=2)))
-    #Define single transpose#
+    # Define single transpose#
     tran = lambda x, filters, strides : layers.Conv3DTranspose(filters=int(filters), strides=(1,strides, strides), **kwargs)(x)
-    #Define transpose block#
+    # Define transpose block#
     tran2 = lambda filters, x : relu(norm(tran(x, filters, strides=2)))
     concat = lambda a, b : layers.Concatenate()([a, b])
-    #Define Dense Block#
+    # Define Dense Block#
     def dense_block(filters,input,DB_depth):
         ext = 2+DB_depth
         outside_layer = input
@@ -97,7 +97,7 @@ def dense_unet(inputs, filters=32):
         C = concat(TU,td_input)
         DB = dense_block(conv1_filters,C, DB_depth)
         return DB
-    #Build Model#
+    # Build Model#
     # TD = convolutions that train down, DB = Dense blocks, TU = Transpose convolutions that train up, C = concatenation groups.
     
     TD1 = td_block(filters*1,filters*1, inputs['dat'],0)
@@ -105,12 +105,12 @@ def dense_unet(inputs, filters=32):
     TD3 = td_block(filters*2,filters*1.5,TD2,2)
     TD4 = td_block(filters*2.5,filters*2,TD3,3)
     TD5 = td_block(filters*3,filters*2.5,TD4,4)
-    #print("TD5 shape: ", TD5.shape)
+    
     TU1 = tu_block(filters*2.5,filters*3,TD5,TD4,4)
     TU2 = tu_block(filters*2,filters*2.5,TU1,TD3,3)
     TU3 = tu_block(filters*1.5,filters*2,TU2,TD2,2)
     TU4 = tu_block(filters*1,filters*1.5,TU3,TD1,1)
-    TU5 = tran2(1, TU4) 
+    TU5 = tran2(filters*1, TU4) 
     logits = {}
     logits['lbl'] = layers.Conv3D(filters = 2, name='lbl', **kwargs)(TU5)
     model = Model(inputs=inputs, outputs=logits )
@@ -154,7 +154,7 @@ def preprocess(self, arrays, **kwargs):
     return arrays
 
 def train():
-    model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath='./ckp/',
+    model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath='./{}/ckp/'.format(p['output_dir']),
         save_weights_only=True,
         monitor='val_dsc',
         mode='max',
@@ -162,8 +162,7 @@ def train():
     
     reduce_lr_callback = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_dsc', factor=0.8, patience=2, mode = "max", verbose = 1)
     early_stop_callback = tf.keras.callbacks.EarlyStopping(monitor='val_dsc', patience=20, verbose=0, mode='max', restore_best_weights=False)
-    tensorboard_callback = tf.keras.callbacks.TensorBoard( 
-        log_dir,       
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir,       
         histogram_freq=1,
         write_images=True,
         write_graph=True,

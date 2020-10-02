44bc30a40cb75242009db00de7e003c1478d65fa
monishramadoss <monish.ramadoss@outlook.com>
2020-10-01 17:27:39 -0700

    refactoring

--- coronary_calcium/Coronary_Segmentation/make_csv.py
+++ coronary_calcium/Coronary_Segmentation/make_csv.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd
-def create_hyper_csv(fname='./hyper.csv', overwrite=False):
+def create_hyper_csv(fname='./jmodel/hyper.csv', overwrite=False):
     if os.path.exists(fname) and not overwrite:
         return    
     id = 4
@@ -12,7 +12,7 @@ def create_hyper_csv(fname='./hyper.csv', overwrite=False):
                     for block_scale in [1, 2]:
                         # --- Create exp
                         for fold in range(1):
-                            df['output_dir'].append('{0}/exp/exp_{1}'.format(os.getcwd(), id) )
+                            df['output_dir'].append('{0}/jmodels/exp/exp_{1}'.format(os.getcwd(), id) )
                             df['fold'].append(fold)
                             df['batch_size'].append(8)
                             df['epochs'].append(epochs)
@@ -20,7 +20,7 @@ def create_hyper_csv(fname='./hyper.csv', overwrite=False):
                             df['block_scale'].append(block_scale)
                             df['alpha'].append(alpha)
                             df['beta'].append(beta)
-                            os.makedirs('{0}/exp/exp_{1}'.format(os.getcwd(), id), exist_ok=True)
+                            os.makedirs('{0}/jmodels/exp/exp_{1}'.format(os.getcwd(), id), exist_ok=True)
 
                             id+=1
 
--- coronary_calcium/Heart_Segmentation/make_csv.py
+++ coronary_calcium/Heart_Segmentation/make_csv.py
@@ -1,9 +1,9 @@
 import os
 import pandas as pd
-def create_hyper_csv(fname='./hyper.csv', overwrite=False):
+def create_hyper_csv(fname='./jmodels/hyper.csv', overwrite=False):
     if os.path.exists(fname) and not overwrite:
         return    
-    id = 4
+    id = 0
     df = {'output_dir': [], 'fold': [], 'batch_size': [], 'epochs': [], 'filters':[], 'block_scale':[], 'alpha':[], 'beta':[]}
     for alpha in [1.0, 0.3]:
         for beta in [1.0, 0.3]:
@@ -12,7 +12,7 @@ def create_hyper_csv(fname='./hyper.csv', overwrite=False):
                     for block_scale in [1, 2]:
                         # --- Create exp
                         for fold in range(1):
-                            df['output_dir'].append('{0}/exp/exp_{1}'.format(os.getcwd(), id) )
+                            df['output_dir'].append('{0}/jmodels/exp/exp_{1}'.format(os.getcwd(), id) )
                             df['fold'].append(fold)
                             df['batch_size'].append(8)
                             df['epochs'].append(epochs)
@@ -20,7 +20,7 @@ def create_hyper_csv(fname='./hyper.csv', overwrite=False):
                             df['block_scale'].append(block_scale)
                             df['alpha'].append(alpha)
                             df['beta'].append(beta)
-                            os.makedirs('{0}/exp/exp_{1}'.format(os.getcwd(), id), exist_ok=True)
+                            os.makedirs('{0}/jmodels/exp/exp_{1}'.format(os.getcwd(), id), exist_ok=True)
 
                             id+=1
 
